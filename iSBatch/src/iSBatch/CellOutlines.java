354c203173591fd798a75bc5653e537d240f1d0d
Victor E. A. Caldas <v.e.a.caldas@rug.nl>
2015-04-08 16:26:33 +0200

    Remove antbuilder from project

---
+++ iSBatch/src/iSBatch/CellOutlines.java
@@ -0,0 +1,70 @@
+package iSBatch;
+
+import model.DatabaseModel;
+import model.Experiment;
+import model.FieldOfView;
+import model.FileNode;
+import model.Node;
+import model.Root;
+import model.Sample;
+import operations.Operation;
+
+public class CellOutlines implements Operation {
+
+	public CellOutlines(DatabaseModel treeModel) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String[] getContext() {
+		return new String[]{"All"};	
+	}
+
+	@Override
+	public String getName() {
+		return "Cell Outlines";
+	}
+
+	@Override
+	public boolean setup(Node node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void finalize(Node node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Root root) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Experiment experiment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Sample sample) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(FieldOfView fieldOfView) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(FileNode fileNode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
