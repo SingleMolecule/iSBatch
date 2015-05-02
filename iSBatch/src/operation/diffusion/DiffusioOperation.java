643b54609fb90b4d842c07062d0be1fa095330d4
Victor E. A. Caldas <v.e.a.caldas@rug.nl>
2015-05-02 19:57:13 +0200

    Initial commit

---
+++ iSBatch/src/operation/diffusion/DiffusioOperation.java
@@ -0,0 +1,373 @@
+package operation.diffusion;
+
+
+import ij.gui.PolygonRoi;
+import ij.measure.ResultsTable;
+import ij.plugin.frame.RoiManager;
+
+import java.awt.Polygon;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import filters.GenericFilter;
+import model.DatabaseModel;
+import model.Experiment;
+import model.FieldOfView;
+import model.FileNode;
+import model.Node;
+import model.OperationNode;
+import model.Root;
+import model.Sample;
+import operation.cellIntensity.CellIntensityGUI;
+import operations.Operation;
+import table.ResultsTableSorter;
+
+public class DiffusioOperation implements Operation {
+	private DiffusionOperationGUI dialog;
+	private String channel;
+	private String customSearch;
+	private ArrayList<String> tags;
+	private int maxdistance;
+	private int lookAhead = 1;
+	private double maxStepSize = 8;
+	private double minimumWidth = 0;
+	private double minimumHeight = 0;
+	private boolean showTrajectories = true;
+	private double pixelSize;
+	private double timeInterval;
+	private int NumberOfPoints;
+	private double fitUntil;
+
+	private boolean doAverage;
+	
+	
+	
+	public DiffusioOperation(DatabaseModel treeModel) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String[] getContext() {
+		return new String[] { "All" };
+	}
+
+	public void run(Node node) {
+		// First, get a list of all files to execute
+		String extention = null;
+		// Run Peak Finder
+
+		ArrayList<Node> filenodes = node.getDescendents(new GenericFilter(
+				channel, tags, extention, customSearch));
+		// Generate Averages
+		for (Node currentNode : filenodes) {
+			System.out.println(currentNode.getFieldOfViewName());
+			if (!node.getCellROIPath().isEmpty()) {
+				// Make Output
+				File f = new File(currentNode.getOutputFolder() + File.separator
+						+ "514_flat.tif_PeakTable.csv");
+				if(f.exists()){
+					track(currentNode);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	private void track(Node currentNode) {
+		// Open the results Table with peaks
+		// Just peaks inside cells
+
+		ResultsTable table = new ResultsTable();
+		
+		
+			try {
+				table = ResultsTable.open(currentNode.getOutputFolder() + File.separator
+						+ "514_flat.tif_PeakTable.csv");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+
+		// Table loaded. Just to solve the proble
+		// TODO: Implement it properly
+
+		ResultsTableSorter.sort(table, true, "slice");
+
+		// initialize trajectory column and set step size to 0
+		for (int row = 0; row < table.getCounter(); row++) {
+			table.setValue("trajectory", row, -1);
+			table.setValue("step_size", row, 0);
+		}
+
+		// determine which rows belong to which slice
+		ArrayList<Integer> offsets = new ArrayList<Integer>();
+		offsets.add(0);
+
+		for (int row = 1; row < table.getCounter(); row++) {
+			int s0 = (int) table.getValue("slice", row - 1);
+			int s1 = (int) table.getValue("slice", row);
+
+			if (s0 != s1)
+				offsets.add(row);
+		}
+
+		offsets.add(table.getCounter());
+
+		// find all trajectories
+		int trajectoryCount = 0;
+
+		for (int i = 0; i < offsets.size() - 1; i++) {
+
+			int from = offsets.get(i);
+			int to = offsets.get(i + 1);
+
+			// make a list of all possible links
+			ArrayList<double[]> links = new ArrayList<double[]>();
+
+			for (int row1 = from; row1 < to; row1++) {
+
+				int s1 = (int) table.getValue("slice", row1);
+				double x1 = table.getValue("x", row1);
+				double y1 = table.getValue("y", row1);
+
+				for (int row2 = to; row2 < table.getCounter(); row2++) {
+
+					int s2 = (int) table.getValue("slice", row2);
+					double x2 = table.getValue("x", row2);
+					double y2 = table.getValue("y", row2);
+
+					if (s2 - s1 > lookAhead)
+						break;
+
+					double dx = x2 - x1;
+					double dy = y2 - y1;
+					double dsq = dx * dx + dy * dy;
+
+					if (dsq < maxStepSize * maxStepSize)
+						links.add(new double[] { s2 - s1, dsq, row1, row2, dx,
+								dy });
+
+				}
+
+			}
+
+			// sort all possible links on distance (or slice number)
+			Collections.sort(links, new Comparator<double[]>() {
+
+				@Override
+				public int compare(double[] o1, double[] o2) {
+					if (o1[0] != o2[0])
+						return Double.compare(o1[0], o2[0]);
+
+					return Double.compare(o1[1], o2[1]);
+				}
+
+			});
+
+			// filter out all links that are not possible
+			Set<Integer> linked = new HashSet<Integer>();
+
+			for (double[] link : links) {
+
+				int r1 = (int) link[2];
+				int r2 = (int) link[3];
+				int t1 = (int) table.getValue("trajectory", r1);
+
+				if (!linked.contains(r1) && !linked.contains(r2)) {
+
+					if (t1 == -1) {
+						t1 = trajectoryCount++;
+						table.setValue("trajectory", r1, t1);
+					}
+
+					table.setValue("trajectory", r2, t1);
+					table.setValue("dx", r2, link[4]);
+					table.setValue("dy", r2, link[5]);
+					table.setValue("step_size", r2, Math.sqrt(link[1]));
+					table.setValue("displacement_sq", r2, link[1]);
+
+					linked.add(r1);
+					linked.add(r2);
+				}
+
+			}
+
+		}
+
+		// sort on slice column
+		ResultsTableSorter.sort(table, true, "slice", "trajectory");
+
+		// filter out trajectories based on minimum/maximum bounding box
+		int from = table.getCounter() - 1;
+		double x = table.getValue("x", from);
+		double y = table.getValue("y", from);
+		Rectangle2D.Double boundingBox = new Rectangle2D.Double(x, y, 0, 0);
+
+		for (int row = from - 1; row >= 0; row--) {
+
+			x = table.getValue("x", row);
+			y = table.getValue("y", row);
+
+			if (table.getValue("trajectory", row) == table.getValue(
+					"trajectory", row + 1)) {
+				boundingBox.add(x, y);
+			} else {
+
+				if (boundingBox.getWidth() < minimumWidth
+						|| boundingBox.getHeight() < minimumHeight) {
+
+					// delete rows
+					for (int r = from; r > row; r--)
+						table.deleteRow(r);
+
+				}
+
+				from = row;
+				boundingBox = new Rectangle2D.Double(x, y, 0, 0);
+			}
+
+		}
+
+		// delete particles (rows) that don't belong to any trajectory
+		for (int row = table.getCounter() - 1; row >= 0; row--) {
+			if (table.getValue("trajectory", row) == -1)
+				table.deleteRow(row);
+		}
+		showTrajectories = false;
+		if (showTrajectories) {
+
+			RoiManager roiManager = RoiManager.getInstance();
+
+			if (roiManager == null)
+				roiManager = new RoiManager();
+
+			Polygon poly = new Polygon();
+			x = table.getValue("x", 0);
+			y = table.getValue("y", 0);
+			poly.addPoint((int) x, (int) y);
+
+			for (int row = 1; row < table.getCounter(); row++) {
+
+				x = table.getValue("x", row);
+				y = table.getValue("y", row);
+
+				if (table.getValue("trajectory", row) == table.getValue(
+						"trajectory", row - 1)) {
+					poly.addPoint((int) x, (int) y);
+				} else {
+					roiManager
+							.addRoi(new PolygonRoi(poly, PolygonRoi.POLYLINE));
+
+					poly = new Polygon();
+					poly.addPoint((int) x, (int) y);
+				}
+			}
+
+			roiManager.addRoi(new PolygonRoi(poly, PolygonRoi.POLYLINE));
+			roiManager.run("Show All");
+			roiManager.runCommand("Save", currentNode.getOutputFolder() + File.separator + currentNode.getName()+ ".track.zip" );
+			roiManager.removeAll();
+		}
+
+		// table.show("Results");
+		try {
+
+			table.saveAs(currentNode.getOutputFolder() + File.separator + currentNode.getName()
+					+ "tracks.csv");
+			
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		table.reset();
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Diffusion";
+	}
+
+	@Override
+	public boolean setup(Node node) {
+		dialog = new DiffusionOperationGUI(node);
+		if (dialog.isCanceled())
+			return false;
+		this.channel = dialog.getChannel();
+		this.customSearch = dialog.getCustomSearch();
+		this.tags = dialog.getTags();
+		this.maxStepSize= dialog.getMaxStepSize();
+		this.pixelSize = dialog.getPixelSize();
+		this.timeInterval = dialog.getTimeInterval();
+		this.NumberOfPoints = dialog.getNumberOfPoints();
+		this.fitUntil = dialog.getFitUntil();
+		this.doAverage  = dialog.getDoAverage();
+		return true;
+	}
+
+
+	@Override
+	public void finalize(Node node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Node[] getCreatedNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, String> getParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void visit(Root root) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Experiment experiment) {
+		run(experiment);
+
+	}
+
+	@Override
+	public void visit(Sample sample) {
+		run(sample);
+
+	}
+
+	@Override
+	public void visit(FieldOfView fieldOfView) {
+		run(fieldOfView);
+	}
+
+	@Override
+	public void visit(FileNode fileNode) {
+		run(fileNode);
+
+	}
+
+	@Override
+	public void visit(OperationNode operationNode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ iSBatch/src/operation/diffusion/DiffusionOperationGUI.java
@@ -0,0 +1,561 @@
+package operation.diffusion;
+
+import iSBatch.iSBatchPreferences;
+import ij.IJ;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JTextField;
+
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import model.Node;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JCheckBox;
+
+public class DiffusionOperationGUI extends JDialog implements ActionListener {
+	private String lookAhead;
+	private JButton btnCancel;
+	private JButton btnProcess;
+
+	// ComboBox
+	private JComboBox<String> fileTypeComboBox;
+	private JComboBox<String> channelComboBox;
+	private JComboBox<String> methodComboBox;
+
+	private static final long serialVersionUID = 1L;
+	private String[] channels = new String[] {
+
+	"[Select Channel]", "All", "Acquisition", "Bright Field", "Red", "Green",
+			"Blue", };
+
+	private static final String[] methods = { "[Method]", "Average Images" };
+	private static final String[] dimensions = { "1D", "2D" };
+	private String[] types = new String[] { "[File Type]", "Raw", "Flat" };
+
+	private JTextField customSearchTxtField;
+
+	private boolean canceled = false;
+
+	static JFrame frame;
+	private Node node;
+	private String channel, method, imagePath;
+	private String maxSteps;
+	private String timeInterval;
+	private String pixelSize;
+	private String numberOfPoints;
+	private String fitUntil;
+
+	/*
+	 * Filter variables
+	 */
+
+	public DiffusionOperationGUI(Node node) {
+		setModal(true);
+		setTitle("Focus Lifetimes");
+		frame = new JFrame("Set Backgroung Image");
+		this.node = node;
+
+		setup();
+		display();
+
+	}
+
+	private void setup() {
+	}
+
+	private void display() {
+
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 60, 60, 60, 60, 60, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 14, 23, 0, 23, 0, 0, 0, 0, 0, 0,
+				0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0, 1.0,
+				0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		getContentPane().setLayout(gridBagLayout);
+
+		JLabel lblBatch = new JLabel("Batch: ");
+		lblBatch.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblBatch = new GridBagConstraints();
+		gbc_lblBatch.anchor = GridBagConstraints.NORTH;
+		gbc_lblBatch.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblBatch.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBatch.gridx = 1;
+		gbc_lblBatch.gridy = 0;
+		getContentPane().add(lblBatch, gbc_lblBatch);
+
+		JLabel lblOperation = new JLabel(node.getType() + ": "
+				+ node.toString());
+
+		GridBagConstraints gbc_lblOperation = new GridBagConstraints();
+		gbc_lblOperation.anchor = GridBagConstraints.SOUTH;
+		gbc_lblOperation.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblOperation.insets = new Insets(0, 0, 5, 5);
+		gbc_lblOperation.gridx = 3;
+		gbc_lblOperation.gridy = 0;
+		getContentPane().add(lblOperation, gbc_lblOperation);
+
+		channelComboBox = new JComboBox<String>();
+		channelComboBox.addActionListener(this);
+
+		channelComboBox.setModel(new DefaultComboBoxModel<String>(channels));
+		GridBagConstraints gbc_channelComboBox = new GridBagConstraints();
+		gbc_channelComboBox.gridwidth = 2;
+		gbc_channelComboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_channelComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_channelComboBox.gridx = 1;
+		gbc_channelComboBox.gridy = 1;
+		getContentPane().add(channelComboBox, gbc_channelComboBox);
+
+		fileTypeComboBox = new JComboBox<String>();
+		fileTypeComboBox.addActionListener(this);
+		fileTypeComboBox.setEditable(true);
+		fileTypeComboBox.setModel(new DefaultComboBoxModel<String>(types));
+		GridBagConstraints gbc_fileTypeComboBox = new GridBagConstraints();
+		gbc_fileTypeComboBox.gridwidth = 2;
+		gbc_fileTypeComboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_fileTypeComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_fileTypeComboBox.gridx = 3;
+		gbc_fileTypeComboBox.gridy = 1;
+		getContentPane().add(fileTypeComboBox, gbc_fileTypeComboBox);
+
+		JLabel lblMethod = new JLabel("Extension: ");
+		GridBagConstraints gbc_lblMethod = new GridBagConstraints();
+		gbc_lblMethod.gridwidth = 2;
+		gbc_lblMethod.anchor = GridBagConstraints.EAST;
+		gbc_lblMethod.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMethod.gridx = 1;
+		gbc_lblMethod.gridy = 2;
+		getContentPane().add(lblMethod, gbc_lblMethod);
+
+		methodComboBox = new JComboBox<String>();
+		methodComboBox.addActionListener(this);
+		methodComboBox.setEditable(true);
+		methodComboBox.setModel(new DefaultComboBoxModel<String>(methods));
+
+		GridBagConstraints gbc_methodComboBox = new GridBagConstraints();
+		gbc_methodComboBox.gridwidth = 2;
+		gbc_methodComboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_methodComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_methodComboBox.gridx = 3;
+		gbc_methodComboBox.gridy = 2;
+		getContentPane().add(methodComboBox, gbc_methodComboBox);
+
+		JLabel lblFilenameContains = new JLabel("Custom search: ");
+		GridBagConstraints gbc_lblFilenameContains = new GridBagConstraints();
+		gbc_lblFilenameContains.gridwidth = 2;
+		gbc_lblFilenameContains.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFilenameContains.anchor = GridBagConstraints.EAST;
+		gbc_lblFilenameContains.gridx = 1;
+		gbc_lblFilenameContains.gridy = 3;
+		getContentPane().add(lblFilenameContains, gbc_lblFilenameContains);
+
+		btnProcess = new JButton("Process");
+		btnProcess.addActionListener(this);
+
+		customSearchTxtField = new JTextField();
+		customSearchTxtField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				customSearch = customSearchTxtField.getText();
+			}
+		});
+
+		GridBagConstraints gbc_customSearchTxtField = new GridBagConstraints();
+		gbc_customSearchTxtField.gridwidth = 2;
+		gbc_customSearchTxtField.insets = new Insets(0, 0, 5, 5);
+		gbc_customSearchTxtField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_customSearchTxtField.gridx = 3;
+		gbc_customSearchTxtField.gridy = 3;
+		getContentPane().add(customSearchTxtField, gbc_customSearchTxtField);
+		customSearchTxtField.setColumns(1);
+
+		lblLookAhead = new JLabel("Time interval:");
+		GridBagConstraints gbc_lblLookAhead = new GridBagConstraints();
+		gbc_lblLookAhead.gridwidth = 2;
+		gbc_lblLookAhead.anchor = GridBagConstraints.EAST;
+		gbc_lblLookAhead.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLookAhead.gridx = 1;
+		gbc_lblLookAhead.gridy = 4;
+		getContentPane().add(lblLookAhead, gbc_lblLookAhead);
+
+		timeIntervaltextField = new JTextField();
+		GridBagConstraints gbc_timeIntervaltextField = new GridBagConstraints();
+		gbc_timeIntervaltextField.insets = new Insets(0, 0, 5, 5);
+		gbc_timeIntervaltextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_timeIntervaltextField.gridx = 3;
+		gbc_timeIntervaltextField.gridy = 4;
+		getContentPane().add(timeIntervaltextField, gbc_timeIntervaltextField);
+		timeIntervaltextField.setColumns(10);
+		timeIntervaltextField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				timeInterval = timeIntervaltextField.getText();
+			}
+		});
+
+		lblSlices = new JLabel("s");
+		GridBagConstraints gbc_lblSlices = new GridBagConstraints();
+		gbc_lblSlices.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSlices.gridx = 4;
+		gbc_lblSlices.gridy = 4;
+		getContentPane().add(lblSlices, gbc_lblSlices);
+
+		lblMaxStepSize = new JLabel("Pixel size:");
+		GridBagConstraints gbc_lblMaxStepSize = new GridBagConstraints();
+		gbc_lblMaxStepSize.anchor = GridBagConstraints.EAST;
+		gbc_lblMaxStepSize.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMaxStepSize.gridx = 2;
+		gbc_lblMaxStepSize.gridy = 5;
+		getContentPane().add(lblMaxStepSize, gbc_lblMaxStepSize);
+
+		PixelSizeTextField = new JTextField();
+		GridBagConstraints gbc_PixelSizeTextField = new GridBagConstraints();
+		gbc_PixelSizeTextField.insets = new Insets(0, 0, 5, 5);
+		gbc_PixelSizeTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_PixelSizeTextField.gridx = 3;
+		gbc_PixelSizeTextField.gridy = 5;
+		getContentPane().add(PixelSizeTextField, gbc_PixelSizeTextField);
+		PixelSizeTextField.setColumns(10);
+		PixelSizeTextField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				pixelSize = PixelSizeTextField.getText();
+			}
+		});
+
+		lblPixels = new JLabel("um");
+		GridBagConstraints gbc_lblPixels = new GridBagConstraints();
+		gbc_lblPixels.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPixels.gridx = 4;
+		gbc_lblPixels.gridy = 5;
+		getContentPane().add(lblPixels, gbc_lblPixels);
+
+		lblMinimumNumberOf = new JLabel("Minimum number of points:");
+		GridBagConstraints gbc_lblMinimumNumberOf = new GridBagConstraints();
+		gbc_lblMinimumNumberOf.gridwidth = 2;
+		gbc_lblMinimumNumberOf.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMinimumNumberOf.gridx = 1;
+		gbc_lblMinimumNumberOf.gridy = 6;
+		getContentPane().add(lblMinimumNumberOf, gbc_lblMinimumNumberOf);
+
+		NumberOfPointstextField = new JTextField();
+		NumberOfPointstextField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				numberOfPoints = NumberOfPointstextField.getText();
+			}
+		});
+		
+		GridBagConstraints gbc_NumberOfPointstextField = new GridBagConstraints();
+		gbc_NumberOfPointstextField.insets = new Insets(0, 0, 5, 5);
+		gbc_NumberOfPointstextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_NumberOfPointstextField.gridx = 3;
+		gbc_NumberOfPointstextField.gridy = 6;
+		getContentPane().add(NumberOfPointstextField,
+				gbc_NumberOfPointstextField);
+		NumberOfPointstextField.setColumns(10);
+
+		lblFitUntilall = new JLabel("Fit until (0=all)");
+		GridBagConstraints gbc_lblFitUntilall = new GridBagConstraints();
+		gbc_lblFitUntilall.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFitUntilall.anchor = GridBagConstraints.EAST;
+		gbc_lblFitUntilall.gridx = 2;
+		gbc_lblFitUntilall.gridy = 7;
+		getContentPane().add(lblFitUntilall, gbc_lblFitUntilall);
+
+		FItLimittextField_1 = new JTextField();
+		
+		FItLimittextField_1.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				fitUntil = FItLimittextField_1.getText();
+			}
+		});
+		
+		
+		GridBagConstraints gbc_FItLimittextField_1 = new GridBagConstraints();
+		gbc_FItLimittextField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_FItLimittextField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_FItLimittextField_1.gridx = 3;
+		gbc_FItLimittextField_1.gridy = 7;
+		getContentPane().add(FItLimittextField_1, gbc_FItLimittextField_1);
+		FItLimittextField_1.setColumns(10);
+
+		lblS = new JLabel("s");
+		GridBagConstraints gbc_lblS = new GridBagConstraints();
+		gbc_lblS.insets = new Insets(0, 0, 5, 5);
+		gbc_lblS.gridx = 4;
+		gbc_lblS.gridy = 7;
+		getContentPane().add(lblS, gbc_lblS);
+
+		lblDiffusionDimensionality = new JLabel("Diffusion dimensionality");
+		GridBagConstraints gbc_lblDiffusionDimensionality = new GridBagConstraints();
+		gbc_lblDiffusionDimensionality.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDiffusionDimensionality.anchor = GridBagConstraints.EAST;
+		gbc_lblDiffusionDimensionality.gridx = 2;
+		gbc_lblDiffusionDimensionality.gridy = 8;
+		getContentPane().add(lblDiffusionDimensionality,
+				gbc_lblDiffusionDimensionality);
+
+		dimensionComboBox = new JComboBox<String>();
+		dimensionComboBox.addActionListener(this);
+		dimensionComboBox
+				.setModel(new DefaultComboBoxModel<String>(dimensions));
+		GridBagConstraints gbc_dimensionComboBox = new GridBagConstraints();
+		gbc_dimensionComboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_dimensionComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_dimensionComboBox.gridx = 3;
+		gbc_dimensionComboBox.gridy = 8;
+		getContentPane().add(dimensionComboBox, gbc_dimensionComboBox);
+
+		chckbxAverageAllTrajectories = new JCheckBox("Average all trajectories");
+		chckbxAverageAllTrajectories.addActionListener(this);
+		GridBagConstraints gbc_chckbxAverageAllTrajectories = new GridBagConstraints();
+		gbc_chckbxAverageAllTrajectories.gridwidth = 2;
+		gbc_chckbxAverageAllTrajectories.insets = new Insets(0, 0, 0, 5);
+		gbc_chckbxAverageAllTrajectories.gridx = 2;
+		gbc_chckbxAverageAllTrajectories.gridy = 9;
+		getContentPane().add(chckbxAverageAllTrajectories,
+				gbc_chckbxAverageAllTrajectories);
+		GridBagConstraints gbc_btnProcess = new GridBagConstraints();
+		gbc_btnProcess.insets = new Insets(0, 0, 0, 5);
+		gbc_btnProcess.gridx = 4;
+		gbc_btnProcess.gridy = 9;
+		getContentPane().add(btnProcess, gbc_btnProcess);
+
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(this);
+
+		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+		gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancel.gridx = 5;
+		gbc_btnCancel.gridy = 9;
+		getContentPane().add(btnCancel, gbc_btnCancel);
+
+		pack();
+
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setVisible(true);
+
+	}
+
+	private void run() {
+		//
+		// // get array of Images
+		// ArrayList<Node> images = node.getDescendents(imageFileNodeFilter);
+
+	}
+
+	public static void main(String[] args) {
+		frame = new JFrame();
+
+		new DiffusionOperationGUI(null);
+
+	}
+
+	void error(String msg) {
+		IJ.error("Batch Processor", msg);
+	}
+
+	public boolean isCanceled() {
+		return canceled;
+	}
+
+	private String imageType = null;
+	private String customSearch;
+	private JLabel lblLookAhead;
+	private JTextField timeIntervaltextField;
+	private JLabel lblSlices;
+	private JLabel lblMaxStepSize;
+	private JTextField PixelSizeTextField;
+	private JLabel lblPixels;
+	private JLabel lblMinimumNumberOf;
+	private JTextField NumberOfPointstextField;
+	private JTextField FItLimittextField_1;
+	private JLabel lblFitUntilall;
+	private JLabel lblS;
+	private JLabel lblDiffusionDimensionality;
+	private JComboBox dimensionComboBox;
+	private JCheckBox chckbxAverageAllTrajectories;
+	private boolean doAverage;
+
+	
+	
+
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCancel) {
+			canceled = true;
+			dispose();
+		} else if (e.getSource() == btnProcess) {
+			String foo = (String) fileTypeComboBox.getSelectedItem();
+			if (foo.equalsIgnoreCase("[File Type]")) {
+				this.imageType = null;
+			} else {
+				this.imageType = (String) fileTypeComboBox.getSelectedItem();
+			}
+
+			foo = (String) channelComboBox.getSelectedItem();
+			if (foo.equalsIgnoreCase("[Select Channel]")) {
+				this.channel = "All";
+			} else {
+				this.channel = channels[channelComboBox.getSelectedIndex()];
+			}
+
+			foo = (String) methodComboBox.getSelectedItem();
+			if (foo.equalsIgnoreCase("[Method]")) {
+				this.method = ".tiff";
+			} else {
+				this.method = (String) methodComboBox.getSelectedItem();
+			}
+			customSearch = customSearchTxtField.getText();
+			pixelSize = PixelSizeTextField.getText();
+			timeInterval = timeIntervaltextField.getText();
+			numberOfPoints = NumberOfPointstextField.getText();
+			fitUntil = FItLimittextField_1.getText();
+			
+			
+		
+			
+			
+			run();
+			dispose();
+		} else if (e.getSource() == channelComboBox) {
+			this.channel = channels[channelComboBox.getSelectedIndex()];
+		} else if (e.getSource() == methodComboBox) {
+			this.method = (String) methodComboBox.getSelectedItem();
+		} else if (e.getSource() == fileTypeComboBox) {
+			this.imageType = (String) fileTypeComboBox.getSelectedItem();
+
+		}
+		
+		else if(e.getSource() == chckbxAverageAllTrajectories){
+			boolean isSelected = chckbxAverageAllTrajectories.isSelected();		
+			
+			
+			
+				if (isSelected) {
+					this.doAverage = true;
+
+				} else {
+					this.doAverage = false;
+				}
+			
+		}
+	}
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public String getImageTag() {
+		return imageType;
+	}
+
+	public ArrayList<String> getTags() {
+		ArrayList<String> container = new ArrayList<String>();
+		if (imageType != null) {
+
+			List<String> list = Arrays.asList(imageType.split("\\s*,\\s*"));
+			for (String foo : list) {
+				container.add(foo);
+			}
+			return container;
+		}
+
+		else
+
+			return container;
+
+	}
+
+	public String getExtention() {
+		return method;
+	}
+
+	public String getCustom() {
+		return imagePath;
+	}
+
+	public String getCustomSearch() {
+		return this.customSearch;
+	}
+
+
+	public int getMaxStepSize() {
+		return Integer.parseInt(maxSteps);
+	}
+	
+	public double getPixelSize(){
+		return Double.parseDouble(pixelSize);
+	}
+	public double gettimeInterval(){
+		return Double.parseDouble(timeInterval);
+	}
+	
+	public double getTimeInterval(){
+		return Double.parseDouble(timeInterval);
+	}
+	public int getNumberOfPoints(){
+		return Integer.parseInt(numberOfPoints);
+	}
+	public double getFitUntil(){
+		return Double.parseDouble(fitUntil);
+	}
+
+	public boolean getDoAverage() {
+	
+		return doAverage;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
