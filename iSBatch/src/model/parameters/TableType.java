c38d838d5c5f786451b2dc80b401f7bc97900e8e
Victor E. A. Caldas <v.e.a.caldas@rug.nl>
2015-05-03 13:11:43 +0200

    add Enumeration

---
+++ iSBatch/src/model/parameters/TableType.java
@@ -0,0 +1,38 @@
+package model.parameters;
+package model.parameters;
+
+/**
+ *  Types of Results Table
+ *  <li>{@link #TRACK}</li>
+ *  <li>{@link #TRACE}</li>
+ *  <li>{@link #PEAK}</li>
+ *  <li>{@link #BRIGHT_FIELD}</li>
+ */
+public enum TableType {
+
+	/** Track table */
+	TRACK,
+	/** TRACE table. */
+	TRACE, 
+	/** PEAK table. */
+	PEAK;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Enum#toString()
+	 */
+	public String toString() {
+		switch (this) {
+		case TRACK:
+			return "Track";
+		case TRACE:
+			return "Trace";
+		case PEAK:
+			return "Peak";
+		default:
+			return "Generic";
+		}
+	}
+
+}
