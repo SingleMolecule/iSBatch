f44373a92c5b949a1d94bad189c9ccfbf1caee2e
Victor E. A. Caldas <v.e.a.caldas@rug.nl>
2015-05-08 08:47:58 +0200

    Remove unused packages;

--- iSBatch/src/model/Database.java
+++ iSBatch/src/model/Database.java
@@ -7,10 +7,8 @@ import ij.IJ;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map.Entry;
 
