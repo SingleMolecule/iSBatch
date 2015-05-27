4a2b7ab5cb70465dd67d95f2fdb126f3814503f1
Michiel Punter <cmpunter@gmail.com>
2015-05-27 17:09:24 +0200

    macro operation
    added the macro operation

--- src/macros/MacroDialog2.java
+++ src/macros/MacroDialog2.java
@@ -1,10 +1,17 @@
 package macros;
 
+import ij.IJ;
+
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -12,22 +19,27 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import test.TreeGenerator;
 import utils.ModelUtils;
 import filters.NodeFilterInterface;
-import gui.CodeTextPane;
+import gui.LogPanel;
 import model.DatabaseModel;
 import model.FileNode;
+import model.Importer;
 import model.Node;
 
 public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Node node;
+	private Importer importer;
 	private NodeFilterInterface filter = new NodeFilterInterface() {
 		
 		@Override
@@ -40,12 +52,22 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 				String tag = (String)tagComboBox.getSelectedItem();
 				String channel = (String)channelComboBox.getSelectedItem();
 				String path = fileNode.getPath();
+				String filename = new File(path).getName();
 				String fileChannel = fileNode.getChannel();
+				
+				if (!customTagTextField.getText().isEmpty())
+					tag = customTagTextField.getText();
 				
 				if (path == null || channel == null)
 					return false;
 				
-				return fileChannel.equalsIgnoreCase(channel) && path.contains(tag) && path.endsWith(extension);
+				
+				System.out.println("fileChannel : " + fileChannel + " == " + channel);
+				System.out.println("filename : " + filename + " contains " + tag);
+				System.out.println("filename : " + filename + " endsWith " + extension);
+				System.out.println();
+				
+				return fileChannel.equalsIgnoreCase(channel) && filename.contains(tag) && filename.endsWith(extension);
 			}
 			
 			return false;
@@ -58,7 +80,7 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 	private JComboBox<String> tagComboBox = new JComboBox<String>();
 	private JTextField customTagTextField = new JTextField(10);
 	private JComboBox<String> extensionComboBox = new JComboBox<String>();
-	private CodeTextPane macroTextPane = new CodeTextPane();
+	private JTextArea macroTextPane = new JTextArea(20, 80);
 	private JTextField macroPathTextField = new JTextField(20);
 	private JButton chooseButton = new JButton("Choose");
 	private JButton runButton = new JButton("Run");
@@ -66,7 +88,8 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 	private JCheckBox addToDatabaseCheckBox = new JCheckBox("Add output to database");
 	private JTextField tagTextField = new JTextField(20);
 	private JCheckBox overrideTagCheckBox = new JCheckBox("Override other tags");
-	
+	private Thread thread;
+	private boolean shouldRun = true;
 	
 	/**
 	 * This is the constructor for the macro dialog. It will show the user a dialog with the following options:
@@ -80,9 +103,11 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 	 * 
 	 * @param node The node on which a macro will be run.
 	 */
-	public MacroDialog2(Node node) {
+	public MacroDialog2(Node node, DatabaseModel model) {
 		this.node = node;
+		this.importer = new Importer(model);
 		
+		macroTextPane.setFont(new Font("Monospaced", Font.PLAIN, 15));
 		setTitle("Run Macro");
 		
 		// fill all combo boxes
@@ -106,7 +131,7 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 		
 		channelComboBox.setModel((ModelUtils.getUniqueChannels(node)));
 		
-
+		tagComboBox.addItem("");
 		for (String tag: tags)
 			tagComboBox.addItem(tag);
 
@@ -119,8 +144,7 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(5, 5, 5, 5);
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-
-
+		
 		gbc.gridx = gbc.gridy = 0;
 		add(new JLabel("Channel"), gbc);
 		
@@ -161,7 +185,7 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 		gbc.gridx = 0;
 		gbc.gridy++;
 		gbc.gridwidth = 4;
-		add(macroTextPane, gbc);
+		add(new JScrollPane(macroTextPane), gbc);
 
 		gbc.gridx = 0;
 		gbc.gridy++;
@@ -188,6 +212,7 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 		add(overrideTagCheckBox, gbc);
 		
 		// set all action listeners
+		chooseButton.addActionListener(this);
 		runButton.addActionListener(this);
 		stopButton.addActionListener(this);
 		
@@ -196,37 +221,121 @@ public class MacroDialog2 extends JDialog implements ActionListener, Runnable {
 		setVisible(true);
 	}
 	
+	public void runMacro(FileNode node, String macro) {
+		
+		System.out.println("RUN MACRO ON " + node);
+		try {
+			
+			URL url = MacroDialog2.class.getResource("/macros/template.ijm");
+			InputStream is = url.openStream();
+			byte[] b = new byte[1024];
+			int len = 0;
+			String template = "";
+			
+			File file = new File(node.getProperty("path"));
+			String name = file.getName();
+			
+			if (name.contains("."))
+				name = name.substring(0, name.indexOf('.'));
+			
+			String tags = "";
+			
+			if (!overrideTagCheckBox.isSelected()) {
+				if (name.contains("_"))
+					tags = name.substring(name.indexOf('_'));
+			}
+			
+			tags += "_" + tagTextField.getText();
+			
+			while ((len = is.read(b)) != -1)
+				template += new String(b, 0, len);
+			
+			
+			String escapedOutputPath = (node.getOutputFolder() + File.separator).replaceAll("\\\\", "\\\\\\\\");
+			String escapedInputPath = node.getProperty("path").replaceAll("\\\\", "\\\\\\\\");
+			
+			System.out.println(escapedInputPath);
+			
+			template = template.replace("%output_path%", escapedOutputPath)
+					.replace("%input_path%", escapedInputPath)
+					.replace("%tags%", tags)
+					.replace("%macro%", macro);
+			
+			LogPanel.log("Run macro on " + node);
+			String paths = IJ.runMacro(template);
+			
+			if (!paths.isEmpty()) {
+				
+				for (String path: paths.split("\n")) {
+					LogPanel.log("Adding " + path + " to " + node);
+					File outputFile = new File(path);
+					importer.importFile(node, outputFile, (String)channelComboBox.getSelectedItem(), outputFile.getName(), outputFile.getPath());
+				}
+				
+			}
+			
+		}
+		catch (IOException e) {
+			LogPanel.log("Could not run macro on node " + node + " : " + e.getMessage());
+		}
+	}
+	
 	public static void main(String[] args) {
 
-		DatabaseModel model = TreeGenerator.generate("e:/test", "e:/test", 2);
-		new MacroDialog2(model.getRoot());
 		
-
+		DatabaseModel model = TreeGenerator.generate("e:/test", "e:/test", 2);
+		new MacroDialog2(model.getRoot(), model);
+		
 	}
 
 	@Override
 	public void run() {
 		
-		for (@SuppressWarnings("unused") Node node: node.getDescendents(filter)) {
+		String macro = macroPathTextField.getText();
+		
+		for (Node node: node.getDescendents(filter)) {
 			
+			if (!shouldRun)
+				return;
 			
+			runMacro((FileNode)node, macro);
 		}
 		
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if (e.getSource() == customTagTextField) {
+		
+		if (e.getSource() == chooseButton) {
+			
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			int option = fileChooser.showOpenDialog(null);
+			
+			if (option == JFileChooser.APPROVE_OPTION) {
+				
+				File file = fileChooser.getSelectedFile();
+				String macro = IJ.openAsString(file.getPath());
+				
+				System.out.println(macro);
+				
+				macroTextPane.setText(macro);
+				
+			}
+			
+		}
+		else if (e.getSource() == customTagTextField) {
 			tagComboBox.setEnabled(customTagTextField.getText().isEmpty());
 		}
 		else if (e.getSource() == runButton) {
-			
-			
-			
+			shouldRun = true;
+			thread = new Thread(this);
+			thread.start();
 		}
 		else if (e.getSource() == stopButton) {
-			
+			shouldRun = false;
 		}
 		
 	}
